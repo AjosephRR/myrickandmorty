4ba5619c0759cfd3a2b14cc6d6f2981887df680c
AjosephRR <66756574+AjosephRR@users.noreply.github.com>
2025-06-13 14:39:33 -0600

    Update MainActivity.kt

--- app/src/main/java/com/ajrr/myrickmorty/MainActivity.kt
+++ app/src/main/java/com/ajrr/myrickmorty/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //IN GITHUB - FIREBASE //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
