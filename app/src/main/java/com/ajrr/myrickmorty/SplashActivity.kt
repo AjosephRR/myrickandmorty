37ae7d38b41ffea7681e906c6a37d3e430650c50
AjosephRR <66756574+AjosephRR@users.noreply.github.com>
2025-06-14 20:28:58 -0600

    Test, sigo en duda
    agregar mas logs para conifrmar el boolean de firebase

--- app/src/main/java/com/ajrr/myrickmorty/SplashActivity.kt
+++ app/src/main/java/com/ajrr/myrickmorty/SplashActivity.kt
@@ -2,7 +2,10 @@ package com.ajrr.myrickmorty
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.ajrr.myrickmorty.auth.LoginActivity
+import com.ajrr.myrickmorty.auth.LoginAlternateActivity
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 
@@ -12,27 +15,26 @@ class SplashActivity : AppCompatActivity () {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Firebase.remoteConfig.setDefaultsAsync(
-            mapOf("use_alternate_login" to false)
-        )
+        Log.d("SPLASH", "Entrando a SplashActivity")
 
-        Firebase.remoteConfig.fetchAndActivate()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val useAlt = Firebase.remoteConfig.getBoolean("use_alternate_login")
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val useAlternate = Firebase.remoteConfig.getBoolean("use_alternate_login")
+                Log.d("SPLASH", "use_alternate_login: $useAlternate")
 
-                    val loginIntent = if (useAlt) {
-                        Intent(this, LoginAlternateActivity::class.java)
-                    } else {
-                        Intent(this, LoginActivity::class.java)
-                    }
-
-                    startActivity(loginIntent)
-                    finish()
+                val intent = if (useAlternate) {
+                    Intent(this, LoginAlternateActivity::class.java)
+                } else {
+                    Intent(this, LoginActivity::class.java)
                 }
+                startActivity(intent)
+                finish()
+            } else {
+                Log.e("SPLASH", "Remote Config fetch failed", task.exception)
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
             }
-
+        }
     }
-
-
 }
