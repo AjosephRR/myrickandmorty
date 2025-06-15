b88b1386f14f9388b981ac2a5ccce704c077e089
AjosephRR <66756574+AjosephRR@users.noreply.github.com>
2025-06-14 20:00:50 -0600

    agregamos Log
    pruebas comunes

--- app/src/main/java/com/ajrr/myrickmorty/data/repository/EpisodeRepository.kt
+++ app/src/main/java/com/ajrr/myrickmorty/data/repository/EpisodeRepository.kt
@@ -1,6 +1,7 @@
 package com.ajrr.myrickmorty.data.repository
 
 import android.content.Context
+import android.util.Log
 import com.ajrr.myrickmorty.data.local.AppDatabase
 import com.ajrr.myrickmorty.data.local.EpisodeEntity
 import com.ajrr.myrickmorty.data.model.Episode
@@ -14,6 +15,7 @@ class EpisodeRepository(context: Context) {
         return try {
             // 1. Obtener de la API
             val remoteEpisodes = RetrofitInstance.api.getEpisodes(page, name).results
+            Log.d("EPISODIOS", "Cargado desde la API (page: $page, name: $name)")
 
             // 2. Guardar en Room (solo si no hay búsqueda)
             if (name.isNullOrEmpty()) {
@@ -22,22 +24,28 @@ class EpisodeRepository(context: Context) {
                 }
                 dao.clearEpisodes()
                 dao.insertAll(entities)
+                Log.d("EPISODIOS", "Guardados en Room (${entities.size} episodios)")
             }
 
             // 3. Retornar lo obtenido
             remoteEpisodes
         } catch (e: Exception) {
-            // 4. Si falla, obtener desde Room
+            Log.e("EPISODIOS", "Error al cargar desde la API: ${e.message}")
+            Log.d("EPISODIOS", "Cargando episodios desde Room...")
+
             val local = dao.getAllEpisodes()
+            Log.d("EPISODIOS", "Leídos ${local.size} episodios desde Room")
+
             local.map {
                 Episode(
                     id = it.id,
                     name = it.name,
                     episode = it.episode,
                     air_date = it.air_date,
-                    characters = emptyList() // Solo si no necesitas personajes locales
+                    characters = emptyList()
                 )
             }
         }
     }
+
 }
